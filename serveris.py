8637c807dc96b20bb551c5362522e29b633fe1e9
Inta Krieviņa <krievinainta@gmail.com>
2020-12-29 09:47:31 +0000

    Skaitļu reizinājums un tipu maiņa

--- serveris.py
+++ serveris.py
@@ -12,5 +12,17 @@ def index():
 def Sveiki():
   return "Nav vairs nekāds rīts!"
 
+@app.route('/Sveiki/<vards>')
+def sveikiPersona(vards):
+  return "Sveiki, {} !".format(vards) #f"Sveiki, {vards}"
+
+@app.route('/cik/<sk1>/<sk2>')
+def reizinajums(sk1,sk2):
+  print(type(sk1))
+  sk1=int(sk1)
+  sk2=int(sk2)
+  reiz=sk1*sk2
+  return str(reiz)
+
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
