b21ec9a0a72d6851e9aa73b3e5828e3ba71f4fc6
Inta Krieviņa <krievinainta@gmail.com>
2020-12-29 10:18:29 +0000

    Meklē vielu ar norādīto ID

--- serveris.py
+++ serveris.py
@@ -1,13 +1,29 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import dati
 
 app = Flask(__name__)
 
+app.config['JSON_AS_ASCII']=False #Latviešu burtiem
 
 @app.route('/')
 def index():
-    return "Labrīt!"
+  print(dati.vielas)
+  return "Labrīt!"
 
+@app.route('/api/v1/vielas')
+def vielas():
+  return jsonify(dati.vielas)
+
+@app.route('/api/v1/viela/<vielasID>')
+def viela(vielasID):
+  viela=f"Viela ar doto ID {vielasID} nav atrasta!"
+  for v in dati.vielas:
+    if v['id']==int(vielasID):
+      viela=str(v)
+  
+    return jsonify(viela)
+  
+    
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
